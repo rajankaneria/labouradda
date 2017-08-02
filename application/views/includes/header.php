42db43d17b855270e5aa0dd87b90c4b6f42bb251
unknown <rajan.kaneria@gmail.com>
2017-08-02 10:31:52 +0530

    Added materialize css cdn

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -7,10 +7,18 @@
     <meta name="msapplication-tap-highlight" content="no">
     <title>CodeIgniter - <?php echo $pageTitle; ?></title>
     <!-- CSS-->
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/css/materialize.min.css">
+
     <link href="<?php echo base_url(); ?>html/css/style.css" rel="stylesheet">
     <link href="http://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet" type="text/css">
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
+    
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/js/materialize.min.js"></script>
+     
     <script src="<?php echo base_url(); ?>html/js/script.js"></script>
     <?php foreach($stylesheet as $fileName){ ?>
     <link href="<?php echo base_url(); ?>html/css/<?php echo $fileName; ?>" rel="stylesheet">
