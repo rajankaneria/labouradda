2be77eb92525baa74b12749f1ad9335e776b92f0
Ravi <rmeswaniya@gmail.com>
2017-08-08 18:29:27 +0530

    team page

--- application/views/team.php
+++ application/views/team.php
@@ -2,15 +2,28 @@
 <div class="container">
 	<div class="c-title">Our Team</div>
 	<p class="c-paragraph">To empower people to do more, to be more</p>
-
-    <div class="row">
-        <div class="col s12 m4">
+</div>
+ <div class="row card-panel">
+    <div class="team-box">
+        <div class="col m4 s12 img-box">
             <div>
-                <img src="<?php echo base_url(); ?>html/images/sid.jpg" />
+                <img src="<?php echo base_url(); ?>html/images/sid.jpeg" class="circle hoverable responsive-img" height="300"/>
             </div>
+        </div><div style="padding: 10px;"></div>
+        <div class="col m8 s12 team-content-box"><p>
+        <strong>SIDDHARTH ASTHANA: CO - FOUNDER & CHIEF INNOVATION OFFICE</strong></p><br><p>Siddharth Asthana is the brain behind the unique innovation of the idea “Labouradda”. He is a professional turned entrepreneur. He is a Co-founder and Director of SID (Shramik Indo Duty) Phones Private Limited. He is a tech fiddler by nature and deep diving in technology has been his hobby, he has worked with GAS – Global Assurance Solutions in Dubai (UAE), Lacoste, has also co-promoted a unique concept to telecom company as an innovative idea that helped in blocking cross border selling. In SID (Shramik Indo Duty) Phones Private Limited, he contributes in Strategic alignment of technology with Labouradda business plan.</p></div>
         </div>
-        <div class="col s12 m6 l3"><p>s12 m6 l3</p></div>
     </div>
 
-</div>
-</div>	
+<div class="row card-panel">
+    <div style="padding: 10px;"></div>
+    <div class="team-box">
+        <div class="col m8 s12 team-content-box"><p>
+        <strong>INDU BHUSHAN ASTHANA: CO-FOUNDER & DIRECTOR</strong></p><br>
+        <p>Indu Bhushan Asthana is the driving force behind the business development & strategic alliances of SID (Shramik Indo Duty) Phones Private Limited. His knowledge from more than TWO decades of experience in several startups in Telecom like Escotel (1996), Reliance (2003), Tata Indicom (2005), Airtel (2006), Idea (2008), Videocon handsets (2010). He has been instrumental in strengthening Lava in UP & Bihar; Gionee in UP & Gujarat & OPPO also in Gujarat. His experience in rolling out business models & team development for operations in the handset industry is unparalleled. Mr. IB is known for maintaining strong and effective Industry relationships with. He has been part of team of brands & companies that have all entered the market as challenger and are today regarded as an epitome of success in the service of GSM/CDMA Network Operators and handsets, terminals and other electronic products. He is currently the Director for Sales & Marketing for “OPPO Brand” and spearheading an operation of 1200 Crore in Gujarat.</p>
+        </div>
+        <div class="col m4 s12 img-box">
+              <img src="<?php echo base_url(); ?>html/images/indu.jpeg" class="circle hoverable responsive-img" height="300"/>
+        </div>
+        </div>
+    </div>
