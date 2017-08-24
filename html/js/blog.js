3b0fe789bff33f49dae5bcd5b86d6150f2ffebf9
Rupal <webservices.intelliworkz@gmail.com>
2017-08-24 15:07:13 +0530

    blog js file

--- html/js/blog.js
+++ html/js/blog.js
@@ -1,14 +1,46 @@
-$("#Blog").on("click",function(){
+/* Insert Records */
+$("#insert").on("click",function(){
 	var baseurl=$('#baseurl').val();
 	var data={
-		'firstname': $("#firstname").val(),
+		'title': $("#title").val(),
 		'author': $('#author').val(),
-		'content':$('#content').val()
-	}
+		'content':$('#content').val(),
+		'feature-image':$('#feature-image').val(),
+		'blog-image':$('#blog-image').val()
+			};
+		$.post(baseurl+"Blog/addBlog",{data:data},function(data){
+				alert("Record has been Inserted successfully");				
 
+		});
+});
+/* Update Records */
+$('#update').on('click',function(){
+var baseurl=$('#baseurl').val(),
+var data={
+	'id':$("#id"),
+	'title':$("#title").val(),
+	'author':$("#author").val(),
+	'content':$("content").val(),
+	'feature-image':$("#feature-image").val(),
+	' blog-image':$("#blog-image").val()
+};
+$.post(baseurl+"Blog/updateBlog",{data:data},function(data){
+	alert("Data has been Updted successfully");
+});
 
+});
+/* Delete Records */
 
+$('#delete').on(click,function(){
+var baseurl=$("#baseurl").val(),
+var data={
+		'title':$("#title").val(),
+		'author':$("#author").val(),
+		'content':$("#content").val(),
+		'feature-image':$("#feature-image").val(),
+		'blog-image':$("#blog-image").val(),
+		'createdon':$("#createdon").val()
 
+};
 
-
-});.
+});
