ed7645bee522e5296dda0d292b4c1ccf5f58a5a6
unknown <rajan.kaneria@gmail.com>
2017-08-24 15:06:03 +0530

    login page design

--- application/views/admin.php
+++ application/views/admin.php
@@ -1,10 +1,14 @@
-<div class="login-title">Login</div>
+
 <div class="row login-form login-cont">
+	
+	<div class="bg-image-overlay"></div>
+	<div class="bg-image"><img src="<?php echo base_url() ?>html/images/slider2.jpg" /></div>
+
 	<div class="col s12">
 		<div class="card-panel login-panel">
 			<div class="form-container">
 				<div class="title">Login</div>
-				<input type="text" name="username" placeholder="User Name" class="login-input" id="inputId">
+				<input type="text" name="username" placeholder="Username" class="login-input" id="inputId">
 				<input type="password" name="password" placeholder="Password" class="login-input" id="inputId">		
 			</div>	
 		</div>
