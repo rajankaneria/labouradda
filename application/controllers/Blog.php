9d818cbb1e8268b8856558705273255bd7d10065
Dimpal Gondaliya <dimpal.v.gondaliyaa14@gmail.com>
2017-08-25 18:51:39 +0530

    update file

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -48,7 +48,7 @@ class Blog extends CI_Controller
 
 
 		//set configuration for the upload library
-		$config['upload_path'] = 'C:\wamp\www\labouradda\html\images\blog';
+		$config['upload_path'] = 'C:\xampp\htdocs\labouradda\html\images\blog';
 	    $config['allowed_types'] = 'gif|jpg|png';
 	    $config['overwrite'] = TRUE;
 	    $config['encrypt_name'] = FALSE;
