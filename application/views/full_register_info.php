99332b979aff94e1e4ce631df6205740cd6dee6d
Ravi <rmeswaniya@gmail.com>
2017-09-29 12:25:23 +0530

    registration info table

--- application/views/full_register_info.php
+++ application/views/full_register_info.php
@@ -15,7 +15,7 @@
         <tr><td><b>Addhar Card :</b> &nbsp; <?php echo $addhar_card; ?></td></tr>          
            <tr><td><b>Phone :</b>&nbsp; <?php echo $phone; ?></td></tr>
           <tr><td><b>Labouradda Location :</b>&nbsp; <?php echo $labouradda_location; ?></td></tr>
-          <div class="row">
+          <tr class="row">
           <td class="col s12 m6">
             <div class="lable-name"><label><b>Addhar Photo :</b></label></div>
             <div>
@@ -26,7 +26,6 @@
             <div class="lable-name"><label><b>Labourer Photo :</b></label></div>
             &nbsp; <img src="<?php echo base_url() ?>html/images/register/<?php echo $labourer_photo; ?>">
           </td>      
-        </div>
         </tr>       
                   
         </tbody>
